19e6af5275276bc419117d862f4c9c8d13801d8f
romantihiy <romantihomirov1@gmail.com>
2022-03-21 00:07:11 +0300

    Added token loading from site

--- src/codeowl/main.js
+++ src/codeowl/main.js
@@ -2,34 +2,40 @@
 define(['base/js/namespace', 'base/js/events'], 
 function(Jupyter, events) 
 {
+    let lastWindow;
     let token = '';
-    let url = 'http://localhost:8000/api';
+    let url = 'http://localhost:8000/';
     let loadingtemplate = 
-    `<img src="https://cdn.dribbble.com/users/225707/screenshots/2958729/jelly-fluid-loader.gif"\n` +
+    `<img src="https://downloader.disk.yandex.ru/preview/d59f6173214890cc2e365e69a8117f0f14dff4fd317e4fa2335c2da98a069023/623766e7/8umrP0U9lF0YtRWsYHcPhBlzbGfmcnKP50OK0V8zMwtBYbA0i1TciwNoCCN6yeA7eHhZVPLjAxTGVFEDB828_g%3D%3D?uid=0&filename=loading.gif&disposition=inline&hash=&limit=0&content_type=image%2Fgif&owner_uid=0&tknv=v2&size=2048x2048"\n` +
     `    alt="Loading..." width="10%" height="10%">\n` +
     `<p> Please wait. You will receive an answer soon... </p>`;
 
-    let callCodeOwl = async function() 
+    let callCodeOwl = async function(check=true)
     {
         let cellOutput = Jupyter.notebook.
             get_selected_cell().output_area.outputs[0];
         let code = '';
-        for (let i = 0; i < code.length; ++i) 
+        for (let i = 0; i < Jupyter.notebook.get_cells().length; ++i) 
         { 
-            cell = Jupyter.notebook.get_cells()[i]
-            code += cell + '\n';
+            let cell = Jupyter.notebook.get_cells()[i];
+            code += cell.get_text() + '\n';
             if (cell == Jupyter.notebook.get_selected_cell())
                 break;
         }
         if (cellOutput.output_type == 'error')
         {
+            if (check)
+            {
+                let check = await checkToken();
+                if (!check) return;
+            }
             Jupyter.notebook.insert_cell_below();
             Jupyter.notebook.select_next();
             Jupyter.notebook.to_markdown();
             let cell = Jupyter.notebook.get_selected_cell();
             cell.set_text(loadingtemplate);
             cell.execute();
-            let response = await fetch(url, 
+            let response = await fetch(url + 'api/', 
             {
                 method: 'POST',
                 headers: {
@@ -45,6 +51,27 @@ function(Jupyter, events)
             cell.execute();
         }
     };
+
+    function getCookie(name) {
+        let matches = document.cookie.match(new RegExp(
+          "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    let checkToken = async function()
+    {
+        token = getCookie('codeowl');
+        let response = await fetch(url + 'validate/', 
+        {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({"token": token})
+        });
+        if (response.ok) return true;
+        lastWindow = window.open(url + 'accounts/login/');
+        return false;
+    }
 
     let codeOwlButton = function () 
     {
@@ -64,13 +91,24 @@ function(Jupyter, events)
         }
     }
 
-    async function load_ipython_extension() 
+    let load_ipython_extension = async function() 
     {
         codeOwlButton();
-        let loader = await fetch('codeowl');
-        if (loader.ok) token = await loader.text();
+        // let loader = await fetch('codeowl');
+        // if (loader.ok) token = await loader.text();
+
+        window.addEventListener("message", function(event) 
+        {
+            if (event.origin + '/' == url)
+            {
+                document.cookie = "codeowl=" + event.data + "; max-age:1800";
+                token = event.data;
+                callCodeOwl(false);
+                lastWindow.close();
+            }
+        });  
         
-        loader = await fetch('codeowl_debug_url');
+        let loader = await fetch('codeowl_debug_url');
         if (loader.ok) url = await loader.text();
     }
 
